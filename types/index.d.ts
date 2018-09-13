0a29b0a650b292189b5be7af3a665ca55b97406d
Piotr Banasik <piotr@jane.app>
2018-09-12 18:09:58 -0700

    Updating type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,13 @@ export interface ResourceCollection {
   register(name: string, path: string, options?: RegisterOptions): void
 }
 
+export interface Collection<T> extends Element {
+  get(id: number): Promise<T>
+  create(data: T): Promise<T>
+  update(id: number, data: T): Promise<T>
+  delete(id: number): Promise<void>
+}
+
 declare const install: () => void
 
 export default install
